1e1592c1e328af96d5ea6937ca09a4b970ca2ec4
JulianAvila259 <julian.avila.967@gmail.com>
2025-11-26 21:17:19 -0500

    Se añade el models calificacion al front

---
+++ src/app/models/calificacion.ts
@@ -0,0 +1,8 @@
+export interface Calificacion {
+  _id?: string;            
+  usuario: string;         
+  receta: string;          
+  puntaje: number;         
+  comentario?: string;     
+  fecha_calificacion?: string; 
+}
